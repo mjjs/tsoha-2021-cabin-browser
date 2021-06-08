7aa7f91b7780004822c97e9c2395d38c5ee155e0
Mikko Siitonen <mikko.siitonen@helsinki.fi>
2021-06-08 23:52:12 +0300

    Move Reservation into reservation_repository file

--- cabin-browser/reservation.py
+++
@@ -1,7 +0,0 @@
-class Reservation:
-    def __init__(self, id, start, end, user_id, cabin_id):
-        self.id = id
-        self.start = start
-        self.end = end
-        self.user_id = user_id
-        self.cabin_id = cabin_id
