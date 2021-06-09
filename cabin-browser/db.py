f4c9d72c0ab1502c7cf763f3c7c837bd4faab83c
Mikko Siitonen <mikko.siitonen@helsinki.fi>
2021-06-09 22:49:12 +0300

    Set autocommit on
    This is only temporary. After the repository calling is moved to a
    service layer, the service layer will be handling the database
    transactions.

--- cabin-browser/db.py
+++ cabin-browser/db.py
@@ -1,14 +1,8 @@
 from psycopg2 import connect, extras
-from flask import g
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 from config import DATABASE_URL
-from user_repository import UserRepository
-from cabin_repository import CabinRepository
-from review_repository import ReviewRepository
-from cabin_image_repository import CabinImageRepository
-from reservation_repository import ReservationRepository
-from municipality_repository import MunicipalityRepository
-from keyword_repository import KeywordRepository
 
 extras.register_uuid()
 
 connection_pool = connect(DATABASE_URL)
+connection_pool.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
