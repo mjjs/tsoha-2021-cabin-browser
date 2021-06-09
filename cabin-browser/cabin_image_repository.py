5c71525291e69b19ee7d28d3224aac2263996ca9
Mikko Siitonen <mikko.siitonen@helsinki.fi>
2021-06-09 22:37:25 +0300

    Extend CabinImageRepository from Repository

--- cabin-browser/cabin_image_repository.py
+++ cabin-browser/cabin_image_repository.py
@@ -1,28 +1,33 @@
-class CabinImageRepository:
+from repository import Repository
+
+
+class CabinImageRepository(Repository):
     def __init__(self, connection_pool):
-        self._connection_pool = connection_pool
+        fields = ["id", "data", "cabin_id", "is_default"]
+        Repository.__init__(
+            self=self,
+            connection_pool=connection_pool,
+            fields=fields,
+            insertable_fields=fields[1:],
+            table_name="cabin_images",
+        )
 
     def add(self, data, cabin_id, is_default):
-        cursor = self._connection_pool.cursor()
-        sql = "INSERT INTO cabin_images(data, cabin_id, is_default) VALUES (%s, %s, %s)"
-        cursor.execute(sql, (data, cabin_id, is_default))
-        self._connection_pool.commit()
-        cursor.close()
+        Repository._add(self=self, values=[data, cabin_id, is_default])
 
     def get(self, id):
-        cursor = self._connection_pool.cursor()
-        sql = "SELECT data FROM cabin_images WHERE id = %s"
-        cursor.execute(sql, (id,))
-        row = cursor.fetchone()
-        cursor.close()
-        return row[0]
+        row = Repository._get(self, id)
+        if not row:
+            return None
+
+        (_, data, _, _) = row
+        return data
 
     def get_by_cabin_id(self, cabin_id):
-        cursor = self._connection_pool.cursor()
-        cursor.execute("SELECT data FROM cabin_images WHERE cabin_id = %s", (cabin_id,))
-        rows = cursor.fetchall()
-        cursor.close()
-        return [r[0] for r in rows] if rows else ["not_found.png"]
+        rows = Repository._get_all(
+            self=self, where_field="cabin_id", where_value=cabin_id
+        )
+        return [data for (_, data, _, _) in rows] if rows else ["not_found.png"]
 
     def get_default_cabin_image(self, cabin_id):
         cursor = self._connection_pool.cursor()
