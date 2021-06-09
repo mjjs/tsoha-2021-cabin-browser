06148cf9f855b15a1ddf3a7f6459b0d32a9a4cb3
Mikko Siitonen <mikko.siitonen@helsinki.fi>
2021-06-09 21:48:33 +0300

    Extend MunicipalityRepository from Repository

--- cabin-browser/municipality_repository.py
+++ cabin-browser/municipality_repository.py
@@ -1,18 +1,24 @@
+from repository import Repository
+
+
 class Municipality:
     def __init__(self, id, name):
         self.id = id
         self.name = name
 
 
-class MunicipalityRepository:
+class MunicipalityRepository(Repository):
     def __init__(self, connection_pool):
-        self._connection_pool = connection_pool
+        Repository.__init__(
+            self=self,
+            connection_pool=connection_pool,
+            fields=["id", "name"],
+            insertable_fields=["name"],
+            table_name="municipalities",
+        )
 
     def get_all(self):
-        cursor = self._connection_pool.cursor()
-        cursor.execute("SELECT id, name FROM municipalities")
-        rows = cursor.fetchall()
-
+        rows = Repository._get_all(self)
         return [Municipality(id, name) for (id, name) in rows]
 
     def get_all_used(self):
